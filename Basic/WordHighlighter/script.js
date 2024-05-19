3b43dc577945803d85bb93e3bb15e8920fe2c189
kosail <kamururi@protonmail.com>
2024-05-19 10:39:36 -0600

    started second DOM project

---
+++ Basic/WordHighlighter/script.js
@@ -0,0 +1,11 @@
+/*
+1. Resaltador de Palabras Clave
+
+Descripción: Crea una página donde los usuarios puedan ingresar una lista de palabras clave. Cuando el usuario haga clic en un botón, todas las palabras en el texto que coincidan con las palabras clave se resaltarán con un color de fondo.
+
+Requisitos:
+
+    Usar document.getElementById para seleccionar el área de texto y el botón.
+    Usar document.querySelectorAll para seleccionar todas las palabras en el texto.
+    Usar element.setAttribute para cambiar el estilo de las palabras coincidentes.
+*/
